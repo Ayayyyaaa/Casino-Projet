6b9544f48465cc414f43a245dfb0667d50107c2d
Ayayyyaaa <florianschlicht000@gmail.com>
2024-10-28 11:34:25 +0100

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,7 +1,5 @@
 # classes.py
 import pygame
-from random import randint
-
 
 
 class Joueur:
@@ -63,17 +61,6 @@ class Coin(pygame.sprite.Sprite):
         self.image = self.sprites[int(self.actuel_sprite)]
 
 
-class Ecran:
-    def __init__(self, actif=False):
-        self.actif = actif
-
-    def get_actif(self):
-        return self.actif
-
-    def set_actif(self, actif):
-        self.actif = actif
-
-
 class Bouton:
     def __init__(self, largeur, hauteur, x, y):
         self.largeur = largeur
@@ -105,95 +92,3 @@ class Bouton:
     def set_y(self, y):
         self.y = y
 
-
-class Piece:
-    def __init__(self):
-        self.cote = None
-        self.choix = None
-
-    def get_cote(self):
-        return self.cote
-
-    def get_choix(self):
-        return self.choix
-
-    def set_cote(self,cote):
-        self.cote = cote
-
-    def set_choix(self, choix):
-        self.choix = choix
-
-    def victoire(self):
-        """
-        permet de determiner si le joueur a gagné
-        """
-        if self.get_cote() == self.get_choix():
-            return True
-        else:
-            return False
-
-
-class Pile_ou_face(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__()
-        self.animation = False
-        self.sprites = []
-        self.sprites.append(pygame.image.load('pistolet_blanc/vide.png'))
-        for j in range(1,17):
-            self.sprites.append(pygame.image.load(f'Pile_ou_face/Piece animation ({j}).png'))
-        self.sprites.append(pygame.image.load(f'Pile_ou_face/Piece animation (1).png'))
-
-        self.actuel_sprite = 0
-        self.image = self.sprites[self.actuel_sprite]
-
-        self.rect = self.image.get_rect()
-        self.rect.topleft = [pos_x, pos_y-100]
-
-    def activer_animation(self):
-        """
-        permet d'activer l'animation
-        """
-        self.animation = True
-
-    def desactiver_animation(self):
-        """
-        permet de desactiver l'animation
-        """
-        self.animation = False
-
-    def pile_ou_face(self):
-        """
-        permet de randomiser le coté de la piece
-        """
-        cote = randint(1, 2)
-        if cote == 1:
-            piece.set_cote('Face')
-        else:
-            piece.set_cote('Pile')
-
-    def update(self, speed, joueur, piece):
-        """
-        permet de jouer l'animation 
-        speed(float): la vitesse de l'animation
-        joueur(object): l'objet joueur1
-        pièce(objet): aled la docu cest chiant quand y a enormement de def aleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed
-        """
-        if self.animation:
-            self.actuel_sprite += speed
-            if int(self.actuel_sprite) >= len(self.sprites):
-                self.actuel_sprite = 0
-                self.pile_ou_face()
-                if piece.victoire():
-                    print(piece.get_choix(), piece.get_cote())
-                    print('vict')
-                    piece.set_choix(None) 
-                    joueur.modifier_cagnotte(100)
-                else:
-                    joueur.modifier_cagnotte(-100)
-                    print('def')
-                    print(piece.get_choix(), piece.get_cote())
-                self.animation = False
-        self.image = self.sprites[int(self.actuel_sprite)]
-
-
-piece = Piece()
